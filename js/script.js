79970e8833ab26a015610a486a2fe6f79816698d
Viewtechlda <kikhadafi@gmail.com>
2021-03-31 12:19:16 +0000

    act

--- js/script.js
+++ js/script.js
@@ -89,3 +89,37 @@ function autoplay(){
 }
 
 let timer=setInterval(autoplay,4000);
+
+   /* Image Slider - Swiper */
+   var imageSlider = new Swiper('.image-slider', {
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+    },
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    breakpoints: {
+        // when window is <= 580px
+        580: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        // when window is <= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window is <= 992px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window is <= 1200px
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        },
+
+    }
+});
